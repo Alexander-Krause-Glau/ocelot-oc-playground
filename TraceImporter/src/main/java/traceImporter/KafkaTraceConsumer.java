e87d6e74365c198b1922ffea1e85cd5bdf6e83f0
Kevin Lotz <lotzk@outlook.com>
2019-12-13 14:03:10 +0100

    Kafka import

--- TraceImporter/src/main/java/traceImporter/KafkaTraceConsumer.java
+++ TraceImporter/src/main/java/traceImporter/KafkaTraceConsumer.java
@@ -1,12 +1,21 @@
 package traceImporter;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import io.opencensus.proto.dump.DumpSpans;
+import io.opencensus.proto.trace.v1.Span;
+import io.opencensus.proto.trace.v1.TraceProto;
 import io.opencensus.trace.SpanBuilder;
 import io.opencensus.trace.SpanId;
+
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Properties;
+
+import io.opencensus.trace.export.SpanData;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -18,7 +27,7 @@ public class KafkaTraceConsumer implements Runnable {
   private static final Logger LOGGER = LoggerFactory.getLogger(KafkaTraceConsumer.class);
   private static final String KAFKA_TOPIC = "cluster-spans";
 
-  private final KafkaConsumer<String, byte[]> kafkaConsumer;
+  private final KafkaConsumer<byte[], byte[]> kafkaConsumer;
 
 
   public KafkaTraceConsumer() {
@@ -28,7 +37,7 @@ public class KafkaTraceConsumer implements Runnable {
     properties.put("group.id", "trace-importer-1");
     properties.put("enable.auto.commit", "true");
     properties.put("auto.commit.interval.ms", "1000");
-    properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");// NOCS
+    properties.put("key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");// NOCS
     properties.put("value.deserializer",
         "org.apache.kafka.common.serialization.ByteArrayDeserializer");
 
@@ -42,38 +51,52 @@ public class KafkaTraceConsumer implements Runnable {
     this.kafkaConsumer.subscribe(Arrays.asList(KAFKA_TOPIC));
 
     while (true) {
-      final ConsumerRecords<String, byte[]> records =
+      final ConsumerRecords<byte[], byte[]> records =
           this.kafkaConsumer.poll(Duration.ofMillis(100));
 
-      for (final ConsumerRecord<String, byte[]> record : records) {
+      for (final ConsumerRecord<byte[], byte[]> record : records) {
 
+        System.out.println("KEY: " + Arrays.toString(record.key()));
         // LOGGER.info("Recevied Kafka record: {}", record.value());
 
         final byte[] serializedTrace = record.value();
 
-        final ByteBuffer buffer = ByteBuffer.wrap(serializedTrace);
+        //System.out.printf("SpanId: %s, TraceId: %s\n", s.getSpanId().toStringUtf8(), s.getTraceId().toStringUtf8());
 
-        int spanIdSize = SpanId.SIZE;
+        try {
+          DumpSpans s = DumpSpans.parseFrom(serializedTrace);
+          System.out.printf("Spans: %d\n", s.getSpansCount());
 
-        final byte[] bytes = new byte[8];
-        buffer.get(bytes);
+          System.out.printf("Span 0 of bundle:\n%s\n\n", SpanToString(s.getSpans(0)));
 
-        ByteBuffer bufferLong = ByteBuffer.allocate(Long.BYTES);
-        bufferLong.put(bytes);
-        bufferLong.flip();// need flip
-        long x = bufferLong.getLong();
-
-        // String spanId = new String(bytes, Charset.forName("UTF-8"));
-
-        // SpanId test = SpanId.fromBytes(bytes);
-
-        // System.out.println(test);
-
-        System.out.println(x);
+        } catch (InvalidProtocolBufferException | UnsupportedEncodingException e) {
+          e.printStackTrace();
+        }
 
       }
     }
 
+  }
+
+
+  private String SpanToString(Span s) throws UnsupportedEncodingException {
+
+    String spanId = Base64.getEncoder().encodeToString(s.getSpanId().toByteArray());
+    String traceId = Base64.getEncoder().encodeToString(s.getTraceId().toByteArray());
+    StringBuilder sb = new StringBuilder("{\n");
+
+
+    sb.append("\tTraceID: ").append(traceId).append("\n");
+    sb.append("\tSpanID: ").append(spanId).append("\n");
+    sb.append("\tname: ").append(s.getName().getValue()).append("\n");
+    sb.append("\tStart (seconds): ").append(s.getStartTime().getSeconds()).append("\n");
+    sb.append("\tEnd (seconds): ").append(s.getEndTime().getSeconds()).append("\n");
+
+    sb.append("}\n");
+
+
+
+    return sb.toString();
   }
 
 }
