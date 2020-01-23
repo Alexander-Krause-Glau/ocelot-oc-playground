e639046a7ed5c383714f7814b744ceb8d27947c1
Alexander Krause <akr@informatik.uni-kiel.de>
2020-01-23 15:08:14 +0100

    wip: single service with kafka streams

---
+++ TraceImporter/trace-reconstructor/src/main/java/traceImporter/SpanToTraceReconstructorStream.java
@@ -0,0 +1,92 @@
+package traceImporter;
+
+import com.google.common.io.BaseEncoding;
+import com.google.protobuf.InvalidProtocolBufferException;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import io.opencensus.proto.dump.DumpSpans;
+import io.opencensus.proto.trace.v1.Span;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import org.apache.kafka.common.serialization.Serdes.StringSerde;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.ForeachAction;
+import org.apache.kafka.streams.kstream.KStream;
+
+/**
+ * Collects spans for 10 seconds, grouped by the trace id, and forwards the resulting batch to the
+ * topic 'span-batches'
+ */
+public class SpanToTraceReconstructorStream {
+
+  private static final String IN_TOPIC = "cluster-dump-spans";
+  private static final String OUT_TOPIC = "span-batches";
+
+  private final Properties streamsConfig = new Properties();
+
+  public SpanToTraceReconstructorStream() {
+
+    streamsConfig.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9091");
+    streamsConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "span-batching");
+
+    streamsConfig.put("schema.registry.url", "http://localhost:8081");
+
+    streamsConfig.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, StringSerde.class);
+    streamsConfig.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+
+  }
+
+
+  public void run() {
+    StreamsBuilder builder = new StreamsBuilder();
+    KStream<byte[], byte[]> dumpSpanStream = builder.stream(IN_TOPIC);
+
+    KStream<String, EVSpan> traceIdSpanStream = dumpSpanStream.flatMap((key, value) -> {
+
+      DumpSpans dumpSpan;
+      List<KeyValue<String, EVSpan>> result = new LinkedList<>();
+      try {
+        dumpSpan = DumpSpans.parseFrom(value);
+
+        for (Span s : dumpSpan.getSpansList()) {
+          String traceId =
+              BaseEncoding.base16().lowerCase().encode(s.getTraceId().toByteArray(), 0, 16);
+
+          String spanId =
+              BaseEncoding.base16().lowerCase().encode(s.getSpanId().toByteArray(), 0, 8);
+
+          result.add(KeyValue.pair(traceId, new EVSpan(spanId, traceId)));
+        }
+
+      } catch (InvalidProtocolBufferException e) {
+        e.printStackTrace();
+      }
+
+      return result;
+
+
+    });
+
+    traceIdSpanStream.foreach(new ForeachAction<String, EVSpan>() {
+      public void apply(String key, EVSpan value) {
+        System.out.println(key + ": " + value.getSpanId());
+      }
+    });
+
+    final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfig);
+    streams.cleanUp();
+    streams.start();
+
+    Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+      @Override
+      public void run() {
+        streams.close();
+      }
+    });
+  }
+
+}
+
