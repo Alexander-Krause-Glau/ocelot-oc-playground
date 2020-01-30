7ac587428cd5a1a312025f23b0725bcc46140567
Alexander Krause <akr@informatik.uni-kiel.de>
2020-01-30 11:06:14 +0100

    trying out ocelot 0.6

--- sampleApplication/runAppMonitoringWithOpenTracing.sh
+++ sampleApplication/runAppMonitoringWithOpenTracing.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -Dinspectit.config.file-based.path="./" -javaagent:inspectit-ocelot-agent-0.4.jar -jar sampleApplication.jar
+java -Dinspectit.config.file-based.path="./" -javaagent:inspectit-ocelot-agent-0.6.jar -jar sampleApplication.jar
 
